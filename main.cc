ce5874ceafd8af143e1d7f319db1eee97ede9dcc
Joshua Mohundro <jmohundro89@gmail.com>
2012-04-16 14:06:02 -0400

    file input and array of caches

--- main.cc
+++ main.cc
@@ -8,6 +8,8 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <fstream>
+#include <stdio.h>
+#include <iostream>
 using namespace std;
 
 #include "cache.h"
@@ -16,7 +18,7 @@ int main(int argc, char *argv[])
 {
 	
 	ifstream fin;
-	FILE * pFile;
+//	FILE * pFile;
 
 	if(argv[1] == NULL){
 		 printf("input format: ");
@@ -25,13 +27,13 @@ int main(int argc, char *argv[])
         }
 
 	/*****uncomment the next five lines*****/
-	//int cache_size = atoi(argv[1]);
-	//int cache_assoc= atoi(argv[2]);
-	//int blk_size   = atoi(argv[3]);
-	//int num_processors = atoi(argv[4]);/*1, 2, 4, 8*/
+	int cache_size = atoi(argv[1]);
+	int cache_assoc= atoi(argv[2]);
+	int blk_size   = atoi(argv[3]);
+	int num_processors = 4;/*1, 2, 4, 8*/
 	//int protocol   = atoi(argv[5]);	 /*0:MSI, 1:MESI, 2:MOESI*/
 	char *fname =  (char *)malloc(20);
- 	fname = argv[6];
+ 	fname = argv[4];
 
 	
 	//****************************************************//
@@ -43,19 +45,42 @@ int main(int argc, char *argv[])
 	//*********************************************//
         //*****create an array of caches here**********//
 	//*********************************************//	
+    Cache *cachesArray[num_processors];
+    
+    for(int k = 0; k <num_processors;k++){
+        cachesArray[k] = new Cache(cache_size,cache_assoc, blk_size);
+    }
+    string line;
+    int processorNumber;
+	uchar operation;
+	ulong address;
 
-	pFile = fopen (fname,"r");
-	if(pFile == 0)
-	{   
-		printf("Trace file problem\n");
-		exit(0);
-	}
+//	pFile = fopen (fname,"r");
+    	ifstream pFile (fname);
+//	if(pFile == 0)
+//	{   
+//		printf("Trace file problem\n");
+//		exit(0);
+//	}
+    if(pFile.is_open()){
+        
+		while(!pFile.eof()){
+			getline(pFile, line, ' ');
+			processorNumber = atoi(line.c_str());
+			if(!(processorNumber >=0 && processorNumber <= 8))break;
+			getline(pFile, line, ' ');
+			operation = line[0];
+			getline(pFile, line, '\n');
+			address = strtoul(line.c_str(), NULL, 16);   
+        }
+    }
+          
 	///******************************************************************//
 	//**read trace file,line by line,each(processor#,operation,address)**//
 	//*****propagate each request down through memory hierarchy**********//
 	//*****by calling cachesArray[processor#]->Access(...)***************//
 	///******************************************************************//
-	fclose(pFile);
+	pFile.close();
 
 	//********************************//
 	//print out all caches' statistics //
